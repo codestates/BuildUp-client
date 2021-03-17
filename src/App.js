07b6ca0682308e0bac02515fe4e849e29d2257ed
kavoom2 <kavoom2@gmail.com>
2021-03-18 00:57:00 +0900

    Close #5 - create dummy server, datas and project settings

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
+import MainPage from "./pages/MainPage";
+import MyPage from "./pages/MyPage";
 
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <div>Hello World</div>
+      <Switch>
+        <Route exact path="/">
+          <MainPage />
+        </Route>
+        <Route exact patch="/user">
+          <MyPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
-// import counterReducer from "../features/counter/counterSlice";
+import { compose, createStore, applyMiddleware } from "redux";
+import rootReducer from "../reducers/index.js";
+import thunk from "redux-thunk";
 
-export default configureStore({
-  reducer: {
-    // counter: counterReducer,
-  },
-});
+// TODO 필요한 Middleware는 배열 안에 넣으세요.
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__; // Chrome Redux Extension 활성화
+const composeEnhancers = devTools || compose;
+const middlewares = [thunk];
+
+// Store Initialization
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(...middlewares)),
+);
+
+export default store;
