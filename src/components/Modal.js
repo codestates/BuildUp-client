d899b77592e46327505f4ba6a6c00195aaacb58d
hsh411 <pixeldot12@gmail.com>
2021-03-18 10:40:54 +0900

    Add login,logout,sign.js from info directory && Fix Modal.js import code

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from "react";
+import login from '../info/Login'
+import logout from '../info/Logout'
+import signin from '../info/Signup'
 
 function Modal() {
   return <div></div>;
