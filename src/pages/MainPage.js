21e489045f4a8a50612414611e5f5303dabc896e
hsh411 <pixeldot12@gmail.com>
2021-03-18 13:41:40 +0900

    Add#1-edit:skeleton

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,5 +2,32 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 export default function MainPage() {
-  return <div></div>;
+  return (
+    <div id="main-page">
+      <header id="header">
+        <div id="header-name">
+          <h1>Build Up</h1>
+        </div>
+        <div id="button-box">
+          <button>Login</button>
+          <button>Join</button>
+        </div>
+      </header>
+      <section>
+        <div id="top-h"></div>
+        <div id="welcome-box">
+          <h1 className="welcome-text">환영합니다.</h1>
+        </div>
+        <div id="btn-beta-box">
+          <button className="btn-beta">
+            <p>체험해보기</p>
+          </button>
+        </div>
+        <div id="top-l"></div>
+      </section>
+      <footer>
+        <div>dd</div>
+      </footer>
+    </div>
+  );
 }
