cf4b8c72cc610084794b065850a9291e9553dd3d
kavoom2 <kavoom2@gmail.com>
2021-03-25 02:49:06 +0900

    Feature#60 - edit: fixTypo

--- src/components/modals/TodoManager.js
+++ src/components/modals/TodoManager.js
@@ -65,7 +65,7 @@ function TodoManager(props) {
   };
   const handleAddTodo = async () => {
     if (word.length === 0) {
-      setAlert("TODO는 최소 1자 이상 입력해야 합니다.");
+      setAlert("최소 1자 이상 입력해야 합니다.");
       return;
     }
     let { year, month, day } = dateSelector;
